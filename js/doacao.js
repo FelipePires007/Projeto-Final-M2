72a8a860e2bf1706ed014bf4c89813bcbc3cbce9
Gabriela <reisgabriela293@gmail.com>
2024-04-11 13:42:10 -0300

    Ajustes

--- js/doacao.js
+++ js/doacao.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
-    $('#donationForm').submit(function(e) {
+  $('#donationForm').submit(function(e) {
       e.preventDefault();
-  
+
       var formData = $(this).serialize();
       var amount = $('#amount').val();
-  
+
       // Simulação de envio do formulário
       // Aqui você pode adicionar a lógica para enviar os dados para o servidor
       setTimeout(function() {
-        $('#donationForm').hide();
-        $('#confirmationMessage').html('Obrigado por sua doação de R$ ' + amount + '!').show();
+          $('#donationForm').hide();
+          $('#confirmationMessage').html('Obrigado por sua doação de R$' + amount +  " reais").show();
+          
+          // Redirecionamento para ../html/links.html após 1 segundo (1000 milissegundos)
+          setTimeout(function() {
+              window.location.href = "../html/links.html";
+          }, 1000);
       }, 1000);
-    });
   });
-  
+});
