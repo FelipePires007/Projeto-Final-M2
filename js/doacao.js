2b70b25831a80cf755a9a59d55a4ee67379b141d
Gabriela <reisgabriela293@gmail.com>
2024-04-11 15:49:51 -0300

    Usuario retorna a página depois de 5 segundos

--- js/doacao.js
+++ js/doacao.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
-  $('#donationForm').submit(function(e) {
-      e.preventDefault();
+    $('#donationForm').submit(function(e) {
+        e.preventDefault();
 
-      var formData = $(this).serialize();
-      var amount = $('#amount').val();
+        var formData = $(this).serialize();
+        var amount = $('#amount').val();
 
-      // Simulação de envio do formulário
-      // Aqui você pode adicionar a lógica para enviar os dados para o servidor
-      setTimeout(function() {
-          $('#donationForm').hide();
-          $('#confirmationMessage').html('Obrigado por sua doação de R$' + amount +  " reais").show();
-          
-          // Redirecionamento para ../html/links.html após 1 segundo (1000 milissegundos)
-          setTimeout(function() {
-              window.location.href = "../html/links.html";
-          }, 1000);
-      }, 1000);
-  });
+        // Simulação de envio do formulário
+        // Aqui você pode adicionar a lógica para enviar os dados para o servidor
+        setTimeout(function() {
+            $('#donationForm').hide();
+            $('#confirmationMessage').html('Obrigado por sua doação de R$' + amount +  " reais").show();
+            
+            // Exibindo mensagem de redirecionamento
+            
+
+            // Redirecionamento para ../html/links.html após 5 segundos (5000 milissegundos)
+            setTimeout(function() {
+                window.location.href = "../html/links.html";
+            }, 5000); // 5 segundos
+        }, 1000); // Tempo para a simulação da doação (1 segundo)
+    });
 });
